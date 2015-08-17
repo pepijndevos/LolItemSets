401c5085151e5288d6568727babb2a3e973f0252
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2015-08-16 18:27:46 -0700

    Initial webapp template

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/lolitemsets.jar clojure.main -m lolitemsets.server
---
+++ env/prod/cljs/lolitemsets/prod.cljs
@@ -0,0 +1,7 @@
+(ns lolitemsets.prod
+  (:require [lolitemsets.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
