210a60bc05acd4da9d04ff06d0a0d15e0ad18627
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2015-08-20 20:23:38 -0700

    Added twitter Bootstrap, slightly sexier page layout

--- src/lolitemsets/core.cljs
+++ src/lolitemsets/core.cljs
@@ -23,18 +23,23 @@
         items (<! (data/item-chan))]
     (swap! app assoc :items items :champs champs)))
 
-(defn champion-select []
-  [:label "Pick a Champion"
-    [:select  {:field :list
-               :on-change (fn [event]
-                            (swap! app
-                              #(assoc %1 :champ (get-in %1 [:champs %2]))
-                              (keyword (-> event .-target .-value))))}
-     (for [[id ch] (sort-by (comp :name val) (:champs @app))]
-       [:option {:key id :value id} (:name ch)])]])
-
 (defn champion-image []
-  [:img {:src (data/champ-img-square-url (get-in @app [:champ :id] "Aatrox"))}])
+  [:img.img-rounded {:src (data/champ-img-square-url (get-in @app [:champ :id] "Aatrox"))
+                     :width 64
+                     :height 64}])
+
+(defn champion-select []
+  [:div.media
+   [:div.media-left (champion-image)]
+   [:div.media-body
+    [:h4.media-heading "Make a build for:"]
+    [:select {:field :list
+              :on-change (fn [event]
+                           (swap! app
+                                  #(assoc %1 :champ (get-in %1 [:champs %2]))
+                                  (keyword (-> event .-target .-value))))}
+     (for [[id ch] (sort-by (comp :name val) (:champs @app))]
+       [:option {:key id :value id} (:name ch)])]]])
 
 (defn recommend []
   (let [{:keys [items num-items champ champ-level props]} @app
@@ -45,13 +50,15 @@
         (recur)))))
 
 (defn item-component [item]
-  [:span
-   [:img {:src (data/item-img-url (get-in item [:image :full]))}]
-   (:name item)])
+  [:div.media
+   [:div.media-left [:img.img-rounded {:src (data/item-img-url (get-in item [:image :full]))
+                                       :width 32 :height 32}]]
+   [:div.media-body
+    [:h4.media-heading (:name item)]]])
 
 (defn item-recommendation []
   [:ul (for [item (:recommended @app)]
-         [:li {:key (:name item)}
+         [:div.well.well-sm {:key (str (:name item) (hash item))}
           (item-component item)])])
 
 (defn objective-checkbox [name objective]
@@ -59,39 +66,39 @@
             (swap! app update-in [:props]
                    (if (-> event .-target .-checked) conj disj)
                    objective))]
-  [:label
-   name
-   [:input {:type :checkbox :on-change toggle}]]))
+    [:div.row
+     [:div.col-xs-1 [:input {:type :checkbox :on-change toggle}]]
+     [:span name]]))
 
 (defn number-selector [label key max min]
-  [:label label
+  [:div.row
+   [:div.col-xs-3 label]
+   [:div.col-xs-9
     [:input {:type "number"
              :field :numeric
              :max max :min min
              :value (key @app)
-             :on-change #(swap! app assoc key (int (-> % .-target .-value)))}]])
+             :on-change #(swap! app assoc key (int (-> % .-target .-value)))}]]])
 
 (defn needlesly-large-button []
-   [:button {:on-click recommend} "Recommend"])
+  [:button {:on-click recommend} "Recommend"])
 
-(defn hello-world []
-  [:div
+(defn app-component []
+  [:div.container
+   [:div.container.col-sm-6
     [:h1 (:text @app)]
-    [:div
-      [champion-select]]
-    [champion-image]
-    [:div
-      [objective-checkbox "Attack damaeg per second" algo/build-dps]
-      [objective-checkbox "Life Steal per second" algo/build-lsps]
-      [objective-checkbox "Ability power" (algo/item-wrapper algo/ability-power)]
-      [objective-checkbox "Effective Health (AP)" algo/build-hp-ap]
-      [objective-checkbox "Effective Health (AD)" algo/build-hp-ad]]
+    [champion-select] [:br]
+    [objective-checkbox "Attack damage per second" algo/build-dps]
+    [objective-checkbox "Life Steal per second" algo/build-lsps]
+    [objective-checkbox "Ability power" (algo/item-wrapper algo/ability-power)]
+    [objective-checkbox "Effective Health (AP)" algo/build-hp-ap]
+    [objective-checkbox "Effective Health (AD)" algo/build-hp-ad] [:br]
     [number-selector "Champion level" :champ-level 18 1]
-    [number-selector "Number of items" :num-items 6 1]
-    [needlesly-large-button]
-    [item-recommendation]])
+    [number-selector "Number of items" :num-items 6 1] [:br]
+    [needlesly-large-button] [:br]]
+   [:div.container.col-sm-6 [item-recommendation]]])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [app-component]
                           (. js/document (getElementById "app")))
 
 
@@ -100,4 +107,3 @@
   ;; your application
   ;; (swap! app update-in [:__figwheel_counter] inc)
 )
-
